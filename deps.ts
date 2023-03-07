7c6090529d935a6ca82cb9c70ecdb9920ec89751
GitHub <noreply@github.com>
2023-03-07 17:12:55 +0000

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from 'https://deno.land/x/oak@v12.0.1/mod.ts';
+import { Application, Router } from 'https://deno.land/x/oak@v12.1.0/mod.ts';
 
 const app = new Application();
 const router = new Router();
